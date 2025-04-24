283d385a828850ed91611b57cc203652108ed64a
Carolina Lihuen Dominguez <carolinaldominguez@outlook.com>
2025-04-24 17:27:30 -0300

    Update basic_R.R

--- basic_R.R
+++ basic_R.R
@@ -1,16 +1,23 @@
 #Probando codigo
+
 #View nos trae la parte de visualización, en este caso es penguins
 View(penguins)
+
 #ggplot nos arma la visualización que aparece en la derecha =>
 ggplot(data=penguins,aes(x=flipper_length_mm,y=body_mass_g))+geom_point(aes(color=species))
+
 #print imprime en consola
 print("Lo veo!")
+
 #variables se asignan con <-
 var_prueba <- "Hola!"
 print(var_prueba)
+
 #vector grupo de elementos del mismo tipo de datos, para aagregarle atributos se usa el c()
 vec_1 <- c(13, 20.5, 18, 12.5, 15,  )
+
 #lubridate => podes ver fechas y horas
+
 #today marca la fecha actual.
 today()
 
@@ -27,5 +34,5 @@ today()
 # else(){}
 # else if(){}
 
-# instalar paquetes con => install.packages()
-# Cargar paquetes con => library(paquete)
+# instalar paquetes con => install.packages("")
+# Cargar paquetes con => library("paquete")
