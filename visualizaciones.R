cf56f270c1ea2431f8d7ce6bc77c2d8fd50af51f
Carolina Lihuen Dominguez <carolinaldominguez@outlook.com>
2025-05-03 20:03:18 -0300

    Update visualizaciones.R

--- visualizaciones.R
+++ visualizaciones.R
@@ -1,2 +1,11 @@
 #Visualizaciones
-#Usando ggplot2
+# Usando ggplot2:
+# -se pueden crear diferentes diagramas
+# -se pueden personificar los datos de los diagramas
+# -se crean altas visualizaciones de calidad
+# -combina la manipulación de datos y la visualización
+
+# Aesthetic : propiedad visual de un objeto del diagrama
+# Geom : objeto geometrico para representar los datos
+# Facets : permite mostrar grupos mas pequeños o subconjuntos de datos
+# Labels o Annotations : te dejan personalizar el diagrama
