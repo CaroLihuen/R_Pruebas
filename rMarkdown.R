ef65baa372c1f3e71d76f346922de0536a127c91
Carolina Lihuen Dominguez <carolinaldominguez@outlook.com>
2025-05-03 20:05:10 -0300

    Create rMarkdown.R

---
+++ rMarkdown.R
@@ -0,0 +1,12 @@
+# R Markdown
+install.packages("rmarkdown")
+# Se instala y verificamos en file
+# Chekear RMD
+
+# Si agregas un # lo toma como un titulo, mientras más # tenga más chiquita va a ser la letra
+# Si agregas < > podes poner un link dentro o colocar una palabra y la página: aqui[link](ww.)
+# Con dos ** entre una palabra agregas negrita
+# Si agregas un * entre una palabra se pone cursiva
+# Se puede insertar código también con la parte llamada chunk
+# Si agregas un * y espacio podes creas puntos para una lista
+# Se puede agregar una imagen con ![nombre de imagen](link de imagen)
